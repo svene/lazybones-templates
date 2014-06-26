253330f494dfde9ed05ba1dafdf45fec32f16641
Sven Ehrke <sven.ehrke@canoo.com>
2014-06-26 21:24:58 +0200

    further work on jee7 template

--- templates/javaee-seven/lazybones.groovy
+++ templates/javaee-seven/lazybones.groovy
@@ -3,10 +3,12 @@ import uk.co.cacoethes.util.NameType
 Map props = [:]
 File projectDir = targetDir instanceof File ? targetDir : new File(String.valueOf(targetDir))
 props.project_name = transformText(projectDir.name, from: NameType.HYPHENATED, to: NameType.PROPERTY)
+
+props.project_group = ask2('group', 'org.example')
+props.project_version = ask2('version', '0.1.0-SNAPSHOT')
+props.project_package_name = ask2('packageName', props.project_group)
+
 props.project_capitalized_name = props.project_name.capitalize()
-props.project_group = ask("Define value for 'group' [org.example]: ", "org.example", "group")
-props.project_version = ask("Define value for 'version' [0.1.0-SNAPSHOT]: ", "0.1.0-SNAPSHOT", "version")
-props.project_package_name = ask("Define value for 'packageName' ["+ props.project_group +"]: ", props.project_group, "packageName")
 
 processTemplates 'build.gradle', props
 processTemplates 'gradle.properties', props
@@ -29,3 +31,6 @@ sources.eachFile { File file ->
 }
 
 
+def ask2(key, proposal) {
+	ask("Define value for '$key' [$proposal]: ", proposal, key)
+}
